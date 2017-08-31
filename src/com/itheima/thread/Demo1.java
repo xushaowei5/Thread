def9d62c6ca14f348f4e221236056c77a2883758
Your Name <875610836@qq.com>
2017-08-31 18:14:16 +0800

    单线程

---
+++ src/com/itheima/thread/Demo1.java
@@ -0,0 +1,27 @@
+package com.itheima.thread;
+
+/**
+ * Created by Administrator on 2017/8/31 0031.
+ */
+public class Demo1 {
+    String name;
+    //带名字的构造
+    Demo1(String name){
+        this.name=name;
+    }
+
+    public void show(){
+        for (int i=0;i<=10001;i++)
+        {
+            System.out.println("name="+name+";"+"i="+i);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Demo1 d1=new Demo1("下滑");
+        Demo1 d2=new Demo1("上升");
+        d1.show();
+        d2.show();
+    }
+}
